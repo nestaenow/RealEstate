7ca25189c719e7a83db17ecbf2bae521c35bdd20
nestaenow <nestaenow@gmail.com>
2022-04-15 11:56:21 +0100

    progress

--- estate/src/components/options.js
+++ estate/src/components/options.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export const options = () => {
+export const Options = ({ options }) => {
+
   return (
-    <div>
-        
+    <div className='options-list flex flex-row'>
+        {options.map((option) => (
+            <div className='box-content w-52 py-8 px-6 bg-white hover:drop-shadow-lg rounded-xl'>
+                <div className='flex flex-row items-center mb-3'>
+                    <p className='text-white bg-primary px-3 py-2 mr-4 rounded-lg text-2xl'>{ option.icon }</p>
+                    <p className='text-darker font-bold text-lg'>{ option.title }</p>
+                </div>
+                <div className='text-gray-400'>{ option.description }</div>
+            </div>
+        ))}
     </div>
   )
 }
--- estate/src/pages/Home.js
+++ estate/src/pages/Home.js
@@ -1,23 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import { LandView } from '../components/LandView'
 import { Navbar } from '../components/Navbar'
+import { Options } from '../components/Options'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShieldHalved } from '@fortawesome/free-solid-svg-icons'
+import { faShieldHalved, faPaperPlane, faLock, faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons'
 
 export const Home = () => {
+    const [options, ] = useState([
+        {title: 'Find Your Dream House', description: 'We provide various types of houses from the cheapest to Premium', icon: <FontAwesomeIcon icon={faPaperPlane}/>},
+        {title: 'Best Quality Guarantee', description: 'If you buy or rent a house with us you are guaranteed with the best quality', icon: <FontAwesomeIcon icon={faShieldHalved}/>},
+        {title: '100% Safe Transactions', description: 'Your transactions will always be kept safe and confidential no matter what', icon: <FontAwesomeIcon icon={faLock}/>},
+        {title: 'Low Cost on Taxes', description: 'Buying or renting a house through our company you get discounts on taxes', icon: <FontAwesomeIcon icon={faFileInvoiceDollar}/>}
+    ])
   return (
-    <div className='mx-8'>
+    <div className='mx-8 mb-8'>
         <Navbar/>
         <LandView/>
-
-        {/* options section */}
-        <div>
-            <div className='flex flex-row items-center'>
-                <p className='text-white bg-primary px-3 py-2 rounded-lg text-2xl'><FontAwesomeIcon icon={faShieldHalved} /></p>
-                <p className=''>Best Quality Gaurantee</p>
-            </div>
-            <div>If you buy or rent a house with us you are gauranteed with the best quality</div>
-        </div>
+        <Options options={options}/>
     </div>
   )
 }
