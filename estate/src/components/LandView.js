823d9b5cfb2a043ec35215987802f007085f7fe1
nestaenow <nestaenow@gmail.com>
2022-04-15 10:00:33 +0100

    progress

--- estate/src/components/LandView.js
+++ estate/src/components/LandView.js
@@ -9,49 +9,56 @@ export const LandView = () => {
             <div>
                 <img src='./images/Estately.png' alt="" srcset="" className='rounded-xl' height={400} width={'100%'}/>
             </div>
-            <div className='absolute bottom-44 ml-32'>
+            <div className='absolute bottom-44 ml-20'>
                 <div className='text-darker font-bold text-6xl mb-5'>
                     Find your next <span className='text-primary'>perfect</span><br/> place with ease
                 </div>
-                <div className='text-gray-400 text-lg mb-14'>
+                <div className='text-gray-400 text-base mb-14'>
                     Estately will help you find your home fast, easy and comfortable. <br/>
                     Our expert support are always available. Let's Start now!
                 </div>
-                <div className='text-primary text-2xl mb-28'>
-                    <div className=' mb-3'>
+                <div className='text-primary text-xl mb-36'>
+                    <div className=' mb-2'>
                         <FontAwesomeIcon icon={faSackDollar} />
-                        <span className='text-dark font-bold text-base mx-2'>$20,5M</span>
-                        <span className='text-gray-400 text-sm'>Earned throught property transactions</span>
+                        <span className='text-dark font-bold text-sm mx-2'>$20,5M</span>
+                        <span className='text-gray-400 text-xs'>Earned throught property transactions</span>
                     </div>
                     <div>
                         <FontAwesomeIcon icon={faHouse} />
-                        <span className='text-dark font-bold text-base mx-2'>19k +</span>
-                        <span className='text-gray-400 text-sm'>Earned throught property transactions</span>
+                        <span className='text-dark font-bold text-sm mx-2'>19k +</span>
+                        <span className='text-gray-400 text-xs'>Earned throught property transactions</span>
                     </div>
                 </div>
             </div>
         </div>
-        <div className='relative justify-between bg-white flex flex-row rounded-r-xl mx-20 p-10 bottom-20 drop-shadow-xl'>
-            <div className='flex flex-col'>
-                <div className='text-gray-400 mb-3'>Location</div>
-                <div className='text-night font-bold text-xl'>Tbilisi</div>
+        <div className='relative  flex flex-col mx-20 bottom-32'>
+            <div className='flex flex-row bg-light box-content w-fit rounded-t-xl'>
+                <div className='px-16 py-4 font-semibold text-base text-gray-500 rounded-t-xl bg-white'>Rent</div>
+                <div className='px-16 py-4 font-semibold text-base text-gray-500'>Buy</div>
+                <div className='px-16 py-4 font-semibold text-base text-gray-500'>Co-Living</div>
             </div>
-            <div className='flex flex-col'>
-                <div className='text-gray-400 mb-3'>Property Type</div>
-                <div className='text-night font-bold text-xl'>Private House</div>
+            <div className='sticky flex flex-row justify-between bg-white p-10 rounded-r-xl rounded-bl-xl  drop-shadow-xl'>
+                <div className='flex flex-col'>
+                    <div className='text-gray-400 mb-3'>Location</div>
+                    <div className='text-night font-bold text-xl'>Tbilisi</div>
+                </div>
+                <div className='flex flex-col'>
+                    <div className='text-gray-400 mb-3'>Property Type</div>
+                    <div className='text-night font-bold text-xl'>Private House</div>
+                </div>
+                <div className='flex flex-col'>
+                    <div className='text-gray-400 mb-3'>Property Size</div>
+                    <div className='text-night font-bold text-xl'>120 m<sup>2</sup>- 150 m<sup>2</sup></div>
+                </div>
+                <div className='flex flex-col'>
+                    <div className='text-gray-400 mb-3'>Property Price Range</div>
+                    <div className='text-night font-bold text-xl'>$85,000 - $98,000</div>
+                </div>
+                
+                <button className='bg-primary text-white px-14 py-4 rounded-md'>
+                    Search
+                </button>
             </div>
-            <div className='flex flex-col'>
-                <div className='text-gray-400 mb-3'>Property Size</div>
-                <div className='text-night font-bold text-xl'>120 m<sup>2</sup>- 150 m<sup>2</sup></div>
-            </div>
-            <div className='flex flex-col'>
-                <div className='text-gray-400 mb-3'>Property Price Range</div>
-                <div className='text-night font-bold text-xl'>$85,000 - $98,000</div>
-            </div>
-            
-            <button className='bg-primary text-white px-14 py-4 rounded-md'>
-                Search
-            </button>
         </div>
       </div>
   )
--- estate/src/pages/Home.js
+++ estate/src/pages/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { LandView } from '../components/LandView'
 import { Navbar } from '../components/Navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShieldHalved } from '@fortawesome/free-solid-svg-icons'
 
 export const Home = () => {
   return (
     <div className='mx-8'>
         <Navbar/>
         <LandView/>
+
+        {/* options section */}
+        <div>
+            <div className='flex flex-row items-center'>
+                <p className='text-white bg-primary px-3 py-2 rounded-lg text-2xl'><FontAwesomeIcon icon={faShieldHalved} /></p>
+                <p className=''>Best Quality Gaurantee</p>
+            </div>
+            <div>If you buy or rent a house with us you are gauranteed with the best quality</div>
+        </div>
     </div>
   )
 }
